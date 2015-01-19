efb067ea31c3f9ff7fcf050ea9681dc8d6c6ba05
Varun Ramesh <varun.ramesh@aol.com>
2015-01-19 14:09:23 -0800

    Generate user id

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,23 @@
 // Ionic Starter App
 
+// Helper function for generating a user ID
+function generateUUID(){
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+};
+
+// If the current user doesn't have an id, generate one
+if(window.localStorage.getItem("userid") === null) {
+    console.log("Generating a user id...");
+    window.localStorage['userid'] = generateUUID();
+}
+var USER_ID = window.localStorage['userid'];
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
