6a60ab203f9c16809fa902071ff3fe0082723aa4
Varun Ramesh <varun.ramesh@aol.com>
2015-01-19 04:15:36 -0800

    Initial code commit

---
+++ www/js/app.js
@@ -0,0 +1,24 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var module = angular.module('starter', ['ionic'])
+module.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+module.controller('HallsCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('http://varunramesh.net:3000/halls').success(function(data) {
+    $scope.halls = data;
+  });
+}]);
